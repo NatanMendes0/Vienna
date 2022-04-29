f9061da0fbeaeefe4dca0d1deb9b156c4ce71350
Natan <natancmendes@gmail.com>
2022-04-28 23:57:06 -0300

    modificações e tambem criação tela produto misoto One

--- css e js/index.js
+++ css e js/index.js
@@ -1,10 +1,10 @@
-const headerButtonStyle = document.body.querySelector('button#header-button-style');
+const headerButtonStyle = document.body.querySelector('button#header-button-style').style;
 
 function mouseEnter(){
-    headerButtonStyle.style.boxShadow = `none`;
-    headerButtonStyle.style.border = `3px solid #49708A`;
+    headerButtonStyle.boxShadow = `none`;
+    headerButtonStyle.border = `3px solid #49708A`;
 }
 function mouseLeave(){
-    headerButtonStyle.style.boxShadow = `0px 4px 4px rgba(0, 0, 0, 0.25)`;
-    headerButtonStyle.style.border = `none`;
+    headerButtonStyle.boxShadow = `0px 4px 4px rgba(0, 0, 0, 0.25)`;
+    headerButtonStyle.border = `none`;
 }
--- php/includes/banco.php
+++ php/includes/banco.php
@@ -1,5 +1,5 @@
 <?php
-
+// conecta o banco de dados
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = new mysqli("localhost", "root", "", "Vienna");
 
