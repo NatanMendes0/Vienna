98176ed7a35e70be98bcbc13a1eaad345d53ab44
Natan Mendes <natamendes.gr062@academico.ifsul.edu.br>
2023-06-13 21:47:22 -0300

    create db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+module.exports = () => {
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    };
+    try {
+        mongoose.connect(process.env.DB_CONNECTION, connectionParams);
+        console.log('Connected to database');
+    }
+    catch (error) {
+        console.log('Could not connect to database');
+    }
+}
